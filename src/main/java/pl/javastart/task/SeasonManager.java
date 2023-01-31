9e8a72c38ae8bd8fbb5c7a7a6ef3b2c41bbb4dab
Maksymilian Żmijewski <makszmijewski1@gmail.com>
2023-01-31 18:03:15 +0100

    pierwszy commit

--- src/main/java/pl/javastart/task/SeasonManager.java
+++ src/main/java/pl/javastart/task/SeasonManager.java
@@ -1,12 +1,27 @@
 package pl.javastart.task;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class SeasonManager {
 
-    // uzupełnij metodę - do wczytywania danych użyj przekazany Scanner
     public void run(Scanner scanner) {
-
+        Season[] seasons = Season.values();
+        System.out.println("Podaj porę roku:");
+        showSeasons(seasons);
+        String userInput = scanner.nextLine();
+        for (Season season : seasons) {
+            if (userInput.equalsIgnoreCase(season.getPolishName())) {
+                System.out.println("W tej porze roku są następujące miesiące:");
+                System.out.println(Arrays.toString(season.getMonths()));
+                break;
+            }
+        }
     }
 
+    private void showSeasons(Season[] seasons) {
+        for (Season season : seasons) {
+            System.out.println(season.getPolishName());
+        }
+    }
 }
